11b3cb9cdf4aaf613049ee2e9786f4c35d5461a3
Ryan Johnson <ryan@Ryan-Johnsons-MacBook-Pro.local>
2010-12-12 15:34:18 -0500

    added main class

--- jquery.view.js
+++ jquery.view.js
@@ -1,11 +1,180 @@
 (function($){
   
   $.View = {
-    isActiveViewInstance: function isViewInstance(object){
+    logging: false,
+    create: function create(structure,methods){
+      var parent_class;
+      if($.View.isViewClass(structure)){
+        parent_class = structure;
+        structure = arguments[1];
+        methods = arguments[2];
+      }
+      var klass = function klass(scope){
+        this._observers = {};
+        this.attributes = {};
+        this.initialize.apply(this,arguments);
+        this.trigger('initialized');
+        if(klass._observers && 'attached' in klass._observers){
+          $.View.triggerOrDelayAttachedEventOnInstance(this);
+        }
+        /* find a smarter way to do this?
+        for(var attribute_name in this){
+          if(typeof(this[attribute_name]) == 'function'){
+            this[attribute_name] = $.proxy(this[attribute_name],this);
+          }
+        }
+        */
+      };
+      klass._observers = {};
+      klass.instance = false;
+      $.extend(klass,$.View.classMethods);
+      if(parent_class){
+        $.extend(klass.prototype,parent_class.prototype);
+        klass.prototype.structure = $.View.wrapFunction(parent_class.prototype.structure,function(proceed){
+          return structure.apply(this,[$.proxy(proceed,this)()]);
+        });
+      }else{
+        $.extend(klass.prototype,$.View.instanceMethods);
+        klass.prototype.structure = structure;
+      }
+      ActiveEvent.extend(klass);
+      klass.prototype.bind = $.View.wrapFunction(klass.prototype.bind,$.View.observeWrapperForAttachedEventOnInstance);
+      if(parent_class){
+        klass._observers = {};
+        for(var observer_name in parent_class._observers){
+          klass._observers[observer_name] = parent_class._observers[observer_name];
+        }
+        klass.prototype._observers = {};
+        $.View.wrapEventMethodsForChildClass(klass,parent_class);
+      }
+      $.extend(klass.prototype,methods || {});
+      return klass;
+      
+    },
+    isViewInstance: function isViewInstance(object){
       return object && object.getElement && object.getElement().nodeType == 1 && object.scope;
     },
-    isActiveViewClass: function isViewClass(object){
+    isViewClass: function isViewClass(object){
       return object && object.prototype && object.prototype.structure && object.prototype.setupScope;
+    },
+    arrayFrom: function arrayFrom(object){
+      if(!object){
+        return [];
+      }
+      var length = object.length || 0;
+      var results = new Array(length);
+      while(length--){
+        results[length] = object[length];
+      }
+      return results;
+    },
+    wrapFunction: function wrapFunction(func,wrapper){
+      return function wrapped(){
+          return wrapper.apply(this,[$.proxy(func,this)].concat($.View.arrayFrom(arguments)));
+      };
+    },
+    nodeInDomTree: function nodeInDomTree(node){
+      var ancestor = node;
+      while(ancestor.parentNode){
+        ancestor = ancestor.parentNode;
+      }
+      return !!(ancestor.body);
+    },
+    wrapEventMethodsForChildClass: function wrapActiveEventMethodsForChildClass(child_class,parent_class){
+      var methods = ['bind','unbind','bindOnce'];
+      for(var i = 0; i < methods.length; ++i){
+        (function method_wrapper_iterator(method_name){
+          parent_class[method_name] = $.View.wrapFunction(parent_class[method_name],function method_wrapper(proceed){
+            var arguments_array = $.View.arrayFrom(arguments).slice(1);
+            child_class[method_name].apply(child_class,arguments_array);
+            return proceed.apply(proceed,arguments_array);
+          });
+        })(methods[i]);
+      }
+    }
+    observeWrapperForAttachedEventOnInstance: function observeWrapperForAttachedEventOnInstance(proceed,event_name){
+      var arguments_array = $.View.arrayFrom(arguments).slice(1);
+      var response = proceed.apply(proceed,arguments_array);
+      if(event_name == 'attached'){
+        $.View.triggerOrDelayAttachedEventOnInstance(this);
+      }
+      return response;
+    },
+    triggerOrDelayAttachedEventOnInstance: function triggerOrDelayAttachedEventOnInstance(instance){
+      if(!instance._attachedEventFired && instance.element && $.View.nodeInDomTree(instance.element)){
+        instance.trigger('attached');
+        instance._attachedEventFired = true;
+        if(instance._attachedEventInterval){
+          clearInterval(instance._attachedEventInterval);
+        }
+      }else if(!('_attachedEventInterval' in instance)){
+        instance._attachedEventInterval = setInterval(function(){
+          if(instance.element && $.View.nodeInDomTree(instance.element)){
+            instance.trigger('attached');
+            instance._attachedEventFired = true;
+            clearInterval(instance._attachedEventInterval);
+            instance._attachedEventInterval = false;
+          }
+        },10);
+      }
+    }
+  };
+  
+  $.View.classMethods = {
+    getInstance: function getInstance(params){
+      if(!this.instance){
+        this.instance = new this(params || {});
+      }
+      return this.instance;
+    },
+    bind: function bind(){
+      
+    },
+    bindOnce: function bindOnce(){
+      
+    },
+    unbind: function unbind(){
+      
+    },
+    trigger: function trigger(){
+      
+    }
+  };
+  
+  $.View.instanceMethods = {
+    initialize: function initialize(attributes){
+      if($.View.logging){
+        console.log('jQuery.View: initialized ',this,' with scope:',scope);
+      }
+      for(var key in attributes){
+        this.set(key,attributes[key]);
+      }
+      var response = this.structure();
+      if(response && !this.element){
+        this.setElement(response);
+      }
+      if(!this.element || !this.element.nodeType || this.element.nodeType !== 1){
+        throw 'The view constructor must return a DOM element, or set this.element as a DOM element. View constructor returned:' + typeof(this.element);
+      }
+      this.trigger('initialized');
+    },
+    bind: $.View.classMethods.bind,
+    bindOnce: $.View.classMethods.bindOnce,
+    unbind: $.View.classMethods.unbind,
+    trigger: function trigger(){
+      
+    },
+    get: function get(key){
+      return this.attributes[key];
+    },
+    set: function set(key,value){
+      return this.attributes[key] = value;
+    },
+    setElement: function setElement(element){
+      this.element = element;
+    },
+    getElement: function getElement(){
+      return this.element;
     }
   };
   
@@ -111,4 +280,11 @@
   };
   
   $.View.Builder.generateTagMethods();
+  $.view = $.View.create;
 })(jQuery);
+
+MyView = $.view(function(){
+  
+},{
+  
+});
